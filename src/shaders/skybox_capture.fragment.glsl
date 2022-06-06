af1c40fd1d0602a6931cb7264caf5699fb966df2
yuanyongxin <yuanyongxin@kedacom.com>
2022-06-06 18:43:19 +0800

    clouds

--- src/shaders/skybox_capture.fragment.glsl
+++ src/shaders/skybox_capture.fragment.glsl
@@ -63,9 +63,10 @@ vec2 density_to_atmosphere(vec3 point, vec3 light_dir) {
     float step_len = ray_len / float(DENSITY_STEPS);
 
     vec2 density_point_to_atmosphere = vec2(0.0);
-    for (int i = 0; i < DENSITY_STEPS; ++i) {
+    for(int i = 0; i < DENSITY_STEPS; ++i) {
         vec3 point_on_ray = point + light_dir * ((float(i) + 0.5) * step_len);
-        density_point_to_atmosphere += local_density(point_on_ray) * step_len;;
+        density_point_to_atmosphere += local_density(point_on_ray) * step_len;
+        ;
     }
 
     return density_point_to_atmosphere;
@@ -79,7 +80,7 @@ vec3 atmosphere(vec3 ray_dir, vec3 sun_direction, float sun_intensity) {
 
     float ray_len = ray_sphere_exit(origin, ray_dir, ATMOSPHERE_RADIUS);
     float step_len = ray_len / float(SAMPLE_STEPS);
-    for (int i = 0; i < SAMPLE_STEPS; ++i) {
+    for(int i = 0; i < SAMPLE_STEPS; ++i) {
         vec3 point_on_ray = origin + ray_dir * ((float(i) + 0.5) * step_len);
 
         // Local density
@@ -117,7 +118,135 @@ const float E = 0.02;
 const float F = 0.30;
 
 vec3 uncharted2_tonemap(vec3 x) {
-   return ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F;
+    return ((x * (A * x + C * B) + D * E) / (x * (A * x + B) + D * F)) - E / F;
+}
+
+#define COVERAGE		0.50
+#define THICKNESS		15.0
+#define ABSORPTION		1.030725
+#define WIND			vec3(0.0, 0.0, -15.0 * 0.2)
+#define FBM_FREQ		2.76434
+#define STEPS			25
+
+#define max_dist 1e8
+const struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+struct Hit {
+    float t;
+    int material_id;
+    vec3 normal;
+    vec3 origin;
+};
+struct Sphere {
+    vec3 origin;
+    float radius;
+    int material;
+};
+
+const vec3 sun_color = vec3(1.0, 0.7, 0.55);
+const Hit no_hit = Hit(float(max_dist + 1e1), -1, vec3(0.0), vec3(0.0));
+const Sphere atm = Sphere(vec3(0.0, -450.0, 0.0), 500.0, 0);
+const Sphere atm2 = Sphere(atm.origin, atm.radius + 50.0, 0);
+
+void intersect_sphere(const in Ray ray, const in Sphere sphere, inout Hit hit) {
+    vec3 rc = sphere.origin - ray.origin;
+    float radius2 = sphere.radius * sphere.radius;
+    float tca = dot(rc, ray.direction);
+    float d2 = dot(rc, rc) - tca * tca;
+    if(d2 > radius2)
+        return;
+    float thc = sqrt(radius2 - d2);
+    float t0 = tca - thc;
+    float t1 = tca + thc;
+    if(t0 < 0.)
+        t0 = t1;
+    if(t0 > hit.t)
+        return;
+    vec3 impact = ray.origin + ray.direction * t0;
+    hit.t = t0;
+    hit.material_id = sphere.material;
+    hit.origin = impact;
+    hit.normal = (impact - sphere.origin) / sphere.radius;
+}
+
+float hash(const in float n) {
+    return fract(sin(n) * 753.5453123);
+}
+
+float noise(const in vec3 x) {
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+    f = f * f * (3.0 - 2.0 * f);
+    float n = p.x + p.y * 157.0 + 113.0 * p.z;
+    return mix(mix(mix(hash(n + 0.0), hash(n + 1.0), f.x), mix(hash(n + 157.0), hash(n + 158.0), f.x), f.y), mix(mix(hash(n + 113.0), hash(n + 114.0), f.x), mix(hash(n + 270.0), hash(n + 271.0), f.x), f.y), f.z);
+}
+
+float fbm(const in vec3 pos, const in float lacunarity) {
+    vec3 p = pos;
+    float t = 0.51749673 * noise(p);
+    p *= lacunarity;
+    t += 0.25584929 * noise(p);
+    p *= lacunarity;
+    t += 0.12527603 * noise(p);
+    p *= lacunarity;
+    t += 0.06255931 * noise(p);
+    return t;
+}
+
+float get_noise(const in vec3 x) {
+    return fbm(x, FBM_FREQ);
+}
+
+float density(const in vec3 pos, const in vec3 offset, const in float t) {
+    vec3 p = pos * .0212242 + offset;
+    float dens = get_noise(p);
+    float cov = 1.0 - COVERAGE;
+    dens *= smoothstep(cov, cov + 0.05, dens);
+    return clamp(dens, 0.0, 1.0);
+}
+
+vec4 renderClouds(Ray eye) {
+    Hit hit = no_hit;
+
+    intersect_sphere(eye, atm, hit);
+
+    const float thickness = THICKNESS;
+
+    const int steps = STEPS;
+
+    float march_step = thickness / float(steps);
+
+    vec3 dir_step = eye.direction / eye.direction.y * march_step;
+    vec3 pos = hit.origin;
+
+    float T = 1.0;
+    vec3 C = vec3(0.0);
+    float alpha = 0.0;
+
+    for(int i = 0; i < steps; i++) {
+        float h = float(i) / float(steps);
+        float dens = density(pos, WIND, h);
+
+        float T_i = exp(-ABSORPTION * dens * march_step);
+
+        T *= T_i;
+
+        if(T < .01)
+            break;
+
+        C += T * (exp(h) / 1.75) * dens * march_step;
+
+        alpha += (1. - T_i) * (1. - alpha);
+
+        pos += dir_step;
+
+        if(length(pos) > 1e3)
+            break;
+    }
+
+    return vec4(C, alpha);
 }
 
 void main() {
@@ -132,6 +261,14 @@ void main() {
     ray_direction.y += y_bias;
 
     vec3 color = atmosphere(normalize(ray_direction), u_sun_direction, u_sun_intensity);
+
+    Ray ray;
+    ray.origin = vec3(0.0);
+    ray.direction = normalize(ray_direction);
+
+    vec4 clouds = renderClouds(ray);
+
+    color = mix(color, clouds.rgb / (0.000001 + clouds.a), clouds.a);
 
     // Apply exposure [3]
     float white_scale = 1.0748724675633854; // 1.0 / uncharted2_tonemap(1000.0)
