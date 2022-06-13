ca6b7128084dec13557e222eb0c7b1c55ab4e91e
yuanyongxin <yuanyongxin@kedacom.com>
2022-06-13 14:59:08 +0800

    修改debug

--- debug/skybox/drawInCanvas.js
+++ debug/skybox/drawInCanvas.js
@@ -2,12 +2,13 @@ export function drawInCanvas(image, width, height) {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
 
+    canvas.style.position = `fixed`;
+    canvas.style.left = `0`;
+    canvas.style.top = `50px`;
     canvas.style.width = `${width}px`;
     canvas.style.height = `${height}px`;
     canvas.width = width;
     canvas.height = height;
-
-    const pixelNum = image.length / 4;
 
     for (let i = 0, len = image.length; i < len; i = i + 4) {
         const r = image[i];
