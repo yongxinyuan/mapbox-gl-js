9d69dce2c9a904780f228235b559a3e74d77c687
yuanyongxin <yuanyongxin@kedacom.com>
2022-06-13 10:46:57 +0800

    copy

--- src/shaders/skybox_capture.fragment.glsl
+++ src/shaders/skybox_capture.fragment.glsl
@@ -123,6 +123,137 @@ vec3 uncharted2_tonemap(vec3 x) {
 
 // 天空盒添加体积云
 // 参考 https://www.shadertoy.com/view/XtBXDw
+// 云层覆盖率
+const float CLOUDS_COVERAGE = 0.50;
+// 云层厚度
+const float CLOUDS_THICKNESS = 10.0;
+// 云层吸收率
+const float CLOUDS_ABSORPTION = 0.930725;
+// 风量影响
+const vec3 CLOUDS_WIND = vec3(0.0, 0.0, -15.0 * 0.2);
+const float CLOUDS_FBM_FREQ = 1.56434;
+// Ray Marching 步数
+const int CLOUDS_STEPS = 25;
+// 最大距离
+const float MAX_DIST = 1e4;
+// 天空盒尺寸
+const vec2 SKY_BOX_SIZE = vec2(512.0, 512.0);
+// 天空盒垂直视角
+const float SKY_BOX_FOV = 45.0;
+
+struct CloudsRay {
+    vec3 origin;
+    vec3 direction;
+};
+struct CloudsHit {
+    float t;
+    vec3 normal;
+    vec3 origin;
+};
+struct CloudsSphere {
+    vec3 origin;
+    float radius;
+};
+
+const CloudsHit noHit = CloudsHit(float(MAX_DIST + 1e1), vec3(0.0), vec3(0.0));
+const CloudsSphere cloudsSphere = CloudsSphere(vec3(0.0, -PLANET_RADIUS, 0.0), ATMOSPHERE_RADIUS);
+
+void intersect_sphere(const in CloudsRay ray, const in CloudsSphere sphere, inout CloudsHit hit) {
+    vec3 rc = sphere.origin - ray.origin;
+    float radius2 = sphere.radius * sphere.radius;
+    float tca = dot(rc, ray.direction);
+    float d2 = dot(rc, rc) - tca * tca;
+    if(d2 > radius2)
+        return;
+    float thc = sqrt(radius2 - d2);
+    float t0 = tca - thc;
+    float t1 = tca + thc;
+    if(t0 < 0.)
+        t0 = t1;
+    if(t0 > hit.t)
+        return;
+    vec3 impact = ray.origin + ray.direction * t0;
+    hit.t = t0;
+    hit.origin = impact;
+    hit.normal = (impact - sphere.origin) / sphere.radius;
+}
+
+float hash(const in float n) {
+    return fract(sin(n) * 753.5453123);
+}
+
+float noise(const in vec3 x) {
+    vec3 p = floor(x);
+    vec3 f = fract(x);
+    f = f * f * (3.0 - 2.0 * f);
+    float n = p.x + p.y * 157.0 + 113.0 * p.z;
+    return mix(mix(mix(hash(n + 0.0), hash(n + 1.0), f.x), mix(hash(n + 157.0), hash(n + 158.0), f.x), f.y), mix(mix(hash(n + 113.0), hash(n + 114.0), f.x), mix(hash(n + 270.0), hash(n + 271.0), f.x), f.y), f.z);
+}
+
+float fbm(const in vec3 pos, const in float lacunarity) {
+    vec3 p = pos;
+    float t = 0.51749673 * noise(p);
+    p *= lacunarity;
+    t += 0.25584929 * noise(p);
+    p *= lacunarity;
+    t += 0.12527603 * noise(p);
+    p *= lacunarity;
+    t += 0.06255931 * noise(p);
+    return t;
+}
+
+float get_noise(const in vec3 x) {
+    return fbm(x, CLOUDS_FBM_FREQ);
+}
+
+float density(const in vec3 pos, const in vec3 offset, const in float t) {
+    vec3 p = pos * .9212242 + offset;
+    float dens = get_noise(p);
+    float cov = 1.0 - CLOUDS_COVERAGE;
+    dens *= smoothstep(cov, cov + 0.05, dens);
+    return clamp(dens, 0.0, 1.0);
+}
+
+vec4 renderClouds(CloudsRay eye) {
+    CloudsHit hit = noHit;
+
+    intersect_sphere(eye, cloudsSphere, hit);
+
+    const float thickness = CLOUDS_THICKNESS;
+    const int steps = CLOUDS_STEPS;
+    float march_step = thickness / float(steps);
+    vec3 dir_step = eye.direction / eye.direction.y * march_step;
+    vec3 pos = hit.origin;
+    float T = 1.0;
+    vec3 C = vec3(0.0);
+    float alpha = 0.0;
+
+    for(int i = 0; i < steps; i++) {
+        float h = float(i) / float(steps);
+        float dens = density(pos, CLOUDS_WIND, h);
+        float T_i = exp(-CLOUDS_ABSORPTION * dens * march_step);
+        T *= T_i;
+        if(T < .01)
+            break;
+        C += T * (exp(h) / 1.75) * dens * march_step;
+        alpha += (1. - T_i) * (1. - alpha);
+        pos += dir_step;
+        if(length(pos) > 1e3)
+            break;
+    }
+
+    return vec4(C, alpha);
+}
+
+CloudsRay getPrimaryRay(const in vec3 cam_local_point, inout vec3 cam_origin, inout vec3 cam_look_at) {
+    vec3 fwd = normalize(cam_look_at - cam_origin);
+    vec3 up = vec3(0, 1, 0);
+    vec3 right = cross(up, fwd);
+    up = cross(fwd, right);
+
+    CloudsRay r = CloudsRay(cam_origin, normalize(fwd + up * cam_local_point.y + right * cam_local_point.x));
+    return r;
+}
 
 void main() {
     vec3 ray_direction = v_position;
@@ -140,6 +271,21 @@ void main() {
     // Apply exposure [3]
     float white_scale = 1.0748724675633854; // 1.0 / uncharted2_tonemap(1000.0)
     color = uncharted2_tonemap((log2(2.0 / pow(u_luminance, 4.0))) * color) * white_scale;
+
+    // 按照示例重新组织空间结构
+    vec2 u_res = SKY_BOX_SIZE;
+    float fov = tan(radians(SKY_BOX_FOV));
+    float pointX = (2.0 * gl_FragCoord.x / u_res.x - 1.0) * u_res.x * 2.0 / u_res.y * fov;
+    float pointY = (2.0 * gl_FragCoord.y / u_res.y - 1.0) * 1.0 * fov;
+    float pointZ = -1.0;
+    vec3 point_cam = vec3(pointX, pointY, pointZ);
+    vec3 eyePosition = vec3(0.0, 0.0, 0.0);
+    vec3 lookAt = vec3(0.0, 0.0, -1.0);
+    CloudsRay eyeRay = getPrimaryRay(point_cam, eyePosition, lookAt);
+
+    vec4 cloudsColor = renderClouds(eyeRay);
+
+    color = mix(color, cloudsColor.rgb / (0.000001 + cloudsColor.a), cloudsColor.a);
 
     gl_FragColor = vec4(color, 1.0);
 }
